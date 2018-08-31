16c4ecfb9e19ec74c22db214a77e3544052c16b5
Baptiste GERONDEAU <bgerondeau@gmail.com>
2018-08-31 13:21:35 +0200

    Decoupling lab logic from bash script

--- files/scripts/benchmark_job.sh
+++ files/scripts/benchmark_job.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 set -ex
 
-helpmsg="benchmark_job.sh --workspace [WORKSPACE] --sftp_user [sftp_user] --node [node] --compiler [compiler] --build-number [build_number] --git-branch [harness_gitbranch] --benchmark [benchmark_name] --compiler_flags compiler_flags --linker_flags linker_flags --harness_options harness_options --run_flags run_flags --iterations iterations --size size"
+helpmsg="benchmark_job.sh --workspace [WORKSPACE] --sftp_path [sftp_path] --sftp_user [sftp_user] --node_type [node_type] --node_name [node_name] --mrp_url [mrp_url] --machine_type [machine_type] --sftp_server_ip [sftp_server_ip]  --compiler [compiler] --build-number [build_number] --ansible-branch [branch for ansible_deploy_harness] --harness-branch [harness_gitbranch] --benchmark [benchmark_name] --compiler_flags compiler_flags --linker_flags linker_flags --harness_options harness_options --run_flags run_flags --iterations iterations --size size "
 
 ARGUMENT_LIST=(
 	"workspace"
-	"node"
+	"sftp_path"
 	"sftp_user"
+	"sftp_server_ip"
+	"node_type"
+	"node_name"
+	"mrp_url"
+	"mrp_token"
+	"machine_type"
 	"compiler"
 	"build_number"
+	"ansible_branch"
 	"harness_branch"
 	"benchmark"
 	"compiler_flags"
 	"linker_flags"
 	"harness_options"
+	"run_flags"
 	"iterations"
 	"size"
-	"run_flags"
 )
 
 root_dir="$(realpath $(dirname $0))"
 
 . ${root_dir}/argparse.sh
 
-if [ ! -n "${workspace}" ] || [ ! -n ${node} ] || [ ! -n ${compiler} ] || [ ! -n ${build_number} ] || [ ! -n ${harness_branch} ] || [ ! -n ${benchmark} ]; then
+if [[ ! -n "${workspace}" ]] || [[ ! -n ${sftp_path} ]] || [[ ! -n ${sftp_user} ]] || [[ ! -n ${sftp_server_ip} ]] || [[ ! -n ${node_type} ]] || [[ ! -n ${node_name} ]] || [[ ! -n ${mrp_url} ]] || [[ ! -n ${machine_type} ]] || [[ ! -n ${compiler} ]] || [[ ! -n ${build_number} ]] || [[ ! -n ${harness_branch} ]] || [[ ! -n ${benchmark} ]] || [[ ! -n ${ansible_branch} ]] || [[ ! -n ${mrp_token} ]]; then
 	echo "Missing Required Argument(s)"
 	echo $helpmsg
 	exit 1
@@ -34,51 +41,14 @@ if [ ! -d "${workspace}" ]; then
 	exit 2
 fi
 
-case "${node}" in
-d03*)
-    vendor='huawei'
-    node_type=d03
-    machine_type=aarch64
-    ;;
-d05*)
-    vendor='huawei'
-    node_type=d05
-    machine_type=aarch64
-    ;;
-qdc*)
-    vendor='qualcomm'
-    node_type=qdc
-    machine_type=aarch64
-    ;;
-tx*)
-    vendor='cavium'
-    node_type=tx2
-    machine_type=aarch64
-    ;;
-x86_64*)
-    vendor='intel'
-    node_type=x86
-    machine_type=x86_64
-    ;;
-*)
-    echo "Unknown node type : ${node} , Exiting..."
-    exit 2
-    ;;
-esac
-if [[ ${compiler} = *"http://"* ]] || [[ ${compiler} = *"ftp://"* ]]; then
-	${root_dir}/tarball_cacher.py ${compiler} /tmp/ --upload=sftp://10.40.0.13/toolchains
-	file=$(basename ${compiler})
-	compiler="http://10.40.0.13/toolchains/${file}"
-fi
-
 cat << EOF > benchmark_job.yml
-mr_provisioner_url: http://10.40.0.11:5000
-mr_provisioner_token: $(cat "/home/${sftp_user}/mrp_token")
-mr_provisioner_machine_name: ${node_type}bench
+mr_provisioner_url: ${mrp_url}
+mr_provisioner_token: ${mrp_token}
+mr_provisioner_machine_name: ${node_name}
 sftp_dirname: ${node_type}-${build_number}
 sftp_user: ${sftp_user}
-sftp_server_ip: 10.40.0.13
-sftp_path: ${vendor}/benchmark
+sftp_server_ip: ${sftp_server_ip}
+sftp_path: ${sftp_path}
 machine_type: ${machine_type}
 branch: ${harness_branch}
 benchmark: ${benchmark}
@@ -95,11 +65,11 @@ if [ -d "${workspace}/ansible-deploy-benchmarks" ]; then
     rm -rf "${workspace}/ansible-deploy-benchmarks"
 fi
 
-git clone -b ${harness_branch} https://github.com/Linaro/ansible-deploy-benchmarks.git "${workspace}/ansible-deploy-benchmarks"
+git clone -b ${ansible_branch} https://github.com/Linaro/ansible-deploy-benchmarks.git "${workspace}/ansible-deploy-benchmarks"
 
 eval `ssh-agent`
 ssh-add
 
-ansible-playbook -v "${workspace}/ansible-deploy-benchmarks/deploy_benchmarks.yml" --extra-vars="@${workspace}/benchmark_job.yml"
+ANSIBLE_CONFIG="${workspace}/ansible-deploy-benchmarks/ansible.cfg" ansible-playbook -v "${workspace}/ansible-deploy-benchmarks/deploy_benchmarks.yml" --extra-vars="@${workspace}/benchmark_job.yml"
 
 ssh-agent -k
